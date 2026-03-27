50d3b05386789a036340f5e2191d7ff50943ea8c
Your Name <you@example.com>
2026-03-26 22:12:18 -0300

    exercício 7

--- src/main/java/org/example/Exercicios/Exercicio7/Exercicio7.java
+++ src/main/java/org/example/Exercicios/Exercicio7/Exercicio7.java
@@ -1,4 +1,12 @@
 package org.example.Exercicios.Exercicio7;
 
 public class Exercicio7 {
+    public static int buscarString(String[] vetor, String nome){
+        for (int i = 0; i < vetor.length; i++){
+            if(vetor[i].equalsIgnoreCase(nome)){
+                return i;
+            }
+        }
+        return -1;
+    }
 }
