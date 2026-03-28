e47d1284360379cd1ee6f2b70bec7a1dea57a599
mmsxdev <hjumiguel@gmail.com>
2026-03-28 19:50:28 -0300

    subindo exercicios 2 e 4

--- src/main/java/org/example/Exercicios/Exercicio4/Exercicio4.java
+++ src/main/java/org/example/Exercicios/Exercicio4/Exercicio4.java
@@ -1,4 +1,30 @@
 package org.example.Exercicios.Exercicio4;
 
 public class Exercicio4 {
+    public boolean VetorOrdenado(int[] vetor) {
+        for (int i = 0; i < vetor.length - 1; i++) {
+            if (vetor[i] > vetor[i + 1]) {
+                return false; // já achou erro
+            }
+        }
+        return true;
+    }
+
+    public int BuscaBinaria(int[] vetor, int valor){
+            int inicio = 0;
+            int fim = vetor.length -1;
+
+            while (inicio <= fim){
+                int meio = (inicio + fim) / 2;
+                if (vetor[meio] == valor){
+                    return meio;
+                }
+                if (vetor[meio] < valor){
+                    inicio = meio + 1;
+                } else {
+                    fim = meio - 1;
+                }
+            }
+            return -1;
+    }
 }
