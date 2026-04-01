27409e6ddbb3022ac35781eb3c06bcb723c1d3cb
Luiz Felipe Alves <luiz2506spike@gmail.com>
2026-04-01 06:50:04 -0300

    exercício 1 e 3

--- src/main/java/org/example/Exercicios/Exercicio1/Exercicio1.java
+++ src/main/java/org/example/Exercicios/Exercicio1/Exercicio1.java
@@ -1,4 +1,15 @@
 package org.example.Exercicios.Exercicio1;
 
 public class Exercicio1 {
+
+    public int buscaSequencial(int[] v, int num) {
+
+        for (int i = 0; i < v.length; i++) {
+            if (v[i] == num) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
 }
