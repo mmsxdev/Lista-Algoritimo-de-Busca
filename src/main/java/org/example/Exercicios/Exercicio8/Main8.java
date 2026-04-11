e96b6a93755531d9aee0869b78d6c91d4d5c7c5e
mmsxdev <hjumiguel@gmail.com>
2026-04-11 13:04:43 -0300

    Pronto Para Enviar!

--- src/main/java/org/example/Exercicios/Exercicio8/Main8.java
+++ src/main/java/org/example/Exercicios/Exercicio8/Main8.java
@@ -4,11 +4,9 @@ import java.util.Scanner;
 
 public class Main8 {
 
-    static Aluno[] alunos = new Aluno[100];
-    static int total = 0;
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Exercicio8 exercicio = new Exercicio8();
         int opcao;
 
         do {
@@ -20,63 +18,16 @@ public class Main8 {
 
             switch (opcao) {
                 case 1:
-                    inserirAluno(sc);
+                    exercicio.inserirAluno(sc);
                     break;
                 case 2:
-                    ordenar();
+                    exercicio.ordenar();
                     break;
                 case 3:
-                    buscar(sc);
+                    exercicio.buscar(sc);
                     break;
             }
 
         } while (opcao != 0);
-    }
-
-    static void inserirAluno(Scanner sc) {
-        sc.nextLine();
-        System.out.print("Nome: ");
-        String nome = sc.nextLine();
-
-        System.out.print("Matrícula: ");
-        int mat = sc.nextInt();
-
-        alunos[total++] = new Aluno(nome, mat);
-    }
-
-    static void ordenar() {
-        for (int i = 0; i < total - 1; i++) {
-            for (int j = 0; j < total - i - 1; j++) {
-                if (alunos[j].matricula > alunos[j + 1].matricula) {
-                    Aluno temp = alunos[j];
-                    alunos[j] = alunos[j + 1];
-                    alunos[j + 1] = temp;
-                }
-            }
-        }
-        System.out.println("Ordenado!");
-    }
-
-    static void buscar(Scanner sc) {
-        System.out.print("Digite a matrícula: ");
-        int mat = sc.nextInt();
-
-        int inicio = 0;
-        int fim = total - 1;
-
-        while (inicio <= fim) {
-            int meio = (inicio + fim) / 2;
-
-            if (alunos[meio].matricula == mat) {
-                System.out.println("Aluno encontrado: " + alunos[meio].nome);
-                return;
-            } else if (alunos[meio].matricula < mat) {
-                inicio = meio + 1;
-            } else {
-                fim = meio - 1;
-            }
-        }
-
-        System.out.println("Aluno não encontrado!");
     }
 }
